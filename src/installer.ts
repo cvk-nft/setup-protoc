2c3c089adf491dcfbbbfef95f57558ec0a2b613f
chris <cvk@clubnft.com>
2022-09-02 21:20:04 -0400

    Evem better!

--- src/installer.ts
+++ src/installer.ts
@@ -186,7 +186,7 @@ async function computeVersion(
   }
 
   const allVersions = await fetchVersions(includePreReleases, repoToken);
-  const validVersions = allVersions.filter(v => semver.valid(v) || semver.valid(normalizeVersion(v)));
+  const validVersions = allVersions.filter(v => semver.valid(v) || semver.valid(normalizeVersion(v).slice(1)));
   const possibleVersions = validVersions.filter(v => v.startsWith(version));
 
   const versionMap = new Map();
